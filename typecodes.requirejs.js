babef4ada809f624bc833944ae592f77fc4495cc
Christopher Buteau <cbuteau@gmail.com>
2019-07-16 17:35:11 -0400

    cleanup

---
+++ typecodes.requirejs.js
@@ -0,0 +1,159 @@
+define('Something/TypeCodes', [], function() {
+
+  var TYPECODES = {
+    BOOLEAN: 0,
+    NUMBER: 1,
+    STRING: 2,
+    FUNCTION: 3,
+    OBJECT: 4,
+    UNDEFINED: 5,
+    NULL: 6,
+    DATE: 7,
+    ARRAY: 8,
+    UNMAPPED: 9,
+    REGEX: 10
+  };
+
+  Object.freeze(TYPECODES);
+
+
+
+  function debugStringForTypeCode(typecode) {
+    var result = 'unmapped';
+    switch (this) {
+      case TYPECODES.ARRAY:
+        result = 'array';
+        break;
+      case TYPECODES.BOOLEAN:
+        result = 'boolean';
+      case TYPECODES.DATE:
+        result = 'date';
+        break;
+      case TYPECODES.FUNCTION:
+        result = 'function';
+        break;
+      case TYPECODES.NULL:
+        result = 'null';
+        break;
+      case TYPECODES.NUMBER:
+        result = 'number';
+        break;
+      case TYPECODES.OBJECT:
+        result = 'object';
+        break;
+      case TYPECODES.REGEX:
+        result = 'regex';
+        break;
+      case TYPECODES.STRING:
+        result = 'string';
+        break;
+      case TYPECODES.UNDEFINED:
+        result = 'undefined';
+        break;
+      case TYPECODES.UNMAPPED:
+      default:
+        result = 'unmapped';
+        break;
+    }
+
+    return result;
+  }
+
+  function getTypeCode(value) {
+    var result;
+    // Undefined
+    if (value === undefined) {
+      //result = TYPECODES.UNDEFINED;
+      //result.toString = debugStringForTypeCode;
+      return TYPECODES.UNDEFINED;
+    }
+    // Null
+    if (value === null) {
+      return TYPECODES.NULL;
+    }
+    // Boolean
+    if ((value === true) || (value === false)) {
+      return TYPECODES.BOOLEAN;
+    }
+    // Function
+    if (value.apply && value.call && value.bind) {
+      return TYPECODES.FUNCTION;
+    }
+    // Date
+    if (value.getDay && value.getYear && value.getMonth && value.getHours && value.getMinutes && value.getSeconds) {
+      return TYPECODES.DATE;
+    }
+    // Number
+    if (value.toString && value.valueOf && value.toPrecision) {
+      return TYPECODES.NUMBER;
+    }
+    // string
+    if (value.trim && value.indexOf && value.toLowerCase && value.toUpperCase) {
+      return TYPECODES.STRING;
+    }
+    // ARRAY
+    if (value.map && value.indexOf && value.push && value.slice) {
+      if (value.copyWithin && value.entries && value.find) {
+        // typedarray
+        // we migth have to swicth to instanceof here.
+        if (value instanceof Array) {
+          console.log('Vanilla Array');
+        } else if (value instanceof TypedArray) {
+          console.log('typed array');
+        }
+      }
+        // we can't get bytes per element at all from the value.
+
+
+      //   switch (value.BYTES_PER_ELEMENT) {
+      //     case 1:
+      //       break;
+      //     case 4:
+      //       break;
+      //     case 8:
+      //       break;
+      //     default:
+      //       console.error('Not found');
+      //       break;
+      //   }
+      // }
+      return TYPECODES.ARRAY;
+    }
+
+    if (value.test && value.exec) {
+      return TYPECODES.REGEX;
+    }
+    // OBJECT
+    if (value.isPrototypeOf && value.hasOwnProperty && value.toString) {
+      return TYPECODES.OBJECT;
+    }
+
+
+    return TYPECODES.UNMAPPED;
+  }
+
+  function isTypeCode(value, code) {
+    return getTypeCode(value) === code;
+  }
+
+  function validateFunction(func, paramCount) {
+    var computed = getTypeCode(func);
+    return (computed === TYPECODES.FUNCTION) && (func.length === paramCount);
+  }
+
+  function isNotThere(value) {
+    var code = getTypeCode(value);
+    return code === TYPECODES.UNDEFINED || code === TYPECODES.NULL;
+  }
+
+  var exposed = {
+    CODES: TYPECODES,
+    get: getTypeCode,
+    is: isTypeCode,
+    isNotThere: isNotThere,
+    validateFunction: validateFunction,
+    str: debugStringForTypeCode
+  };
+
+    return exposed;
+});
