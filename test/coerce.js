6d1a3246b5f221f68ddf3d6db2dcb350ad90d9a0
Christopher Buteau <cbuteau@gmail.com>
2020-03-29 19:33:09 -0400

    moar tests

--- test/coerce.js
+++ test/coerce.js
@@ -1,54 +1,57 @@
 /* es-lint jasmine */
 
 describe('coerce...', function() {
-  describe('bool...', function() {
-    it ('one', function() {
-      expect(tc.coerce('true', tc.CODES.BOOLEAN)).toBe(true);
-    });
-    it ('two', function() {
-      expect(tc.coerce('True', tc.CODES.BOOLEAN)).toBe(true);
-    });
-    it ('three', function() {
-      expect(tc.coerce('tRue', tc.CODES.BOOLEAN)).toBe(true);
-    });
-    it ('four', function() {
-      expect(tc.coerce('trUe', tc.CODES.BOOLEAN)).toBe(true);
-    });
-    it ('five', function() {
-      expect(tc.coerce('truE', tc.CODES.BOOLEAN)).toBe(true);
-    });
+  describe('string...', function() {
+    describe('bool...', function() {
+      it ('one', function() {
+        expect(tc.coerce('true', tc.CODES.BOOLEAN)).toBe(true);
+      });
+      it ('two', function() {
+        expect(tc.coerce('True', tc.CODES.BOOLEAN)).toBe(true);
+      });
+      it ('three', function() {
+        expect(tc.coerce('tRue', tc.CODES.BOOLEAN)).toBe(true);
+      });
+      it ('four', function() {
+        expect(tc.coerce('trUe', tc.CODES.BOOLEAN)).toBe(true);
+      });
+      it ('five', function() {
+        expect(tc.coerce('truE', tc.CODES.BOOLEAN)).toBe(true);
+      });
 
 
-    it ('one', function() {
-      expect(tc.coerce('false', tc.CODES.BOOLEAN)).toBe(false);
-    });
-    it ('two', function() {
-      expect(tc.coerce('fAlse', tc.CODES.BOOLEAN)).toBe(false);
-    });
-    it ('three', function() {
-      expect(tc.coerce('faLse', tc.CODES.BOOLEAN)).toBe(false);
-    });
-    it ('four', function() {
-      expect(tc.coerce('falSe', tc.CODES.BOOLEAN)).toBe(false);
-    });
-    it ('five', function() {
-      expect(tc.coerce('falsE', tc.CODES.BOOLEAN)).toBe(false);
-    });
-  });
-
-  describe('number...',function() {
-    describe('ints...', function() {
-      it ('666', function() {
-        expect(tc.coerce('666', tc.CODES.NUMBER)).toBe(666);
+      it ('one', function() {
+        expect(tc.coerce('false', tc.CODES.BOOLEAN)).toBe(false);
+      });
+      it ('two', function() {
+        expect(tc.coerce('fAlse', tc.CODES.BOOLEAN)).toBe(false);
+      });
+      it ('three', function() {
+        expect(tc.coerce('faLse', tc.CODES.BOOLEAN)).toBe(false);
+      });
+      it ('four', function() {
+        expect(tc.coerce('falSe', tc.CODES.BOOLEAN)).toBe(false);
+      });
+      it ('five', function() {
+        expect(tc.coerce('falsE', tc.CODES.BOOLEAN)).toBe(false);
       });
     });
 
-    describe('floats..', function() {
-      it ('3.14', function() {
-        expect(tc.coerce('3.14', tc.CODES.NUMBER)).toBe(3.14);
+    describe('number...',function() {
+      describe('ints...', function() {
+        it ('666', function() {
+          expect(tc.coerce('666', tc.CODES.NUMBER)).toBe(666);
+        });
+      });
+
+      describe('floats..', function() {
+        it ('3.14', function() {
+          expect(tc.coerce('3.14', tc.CODES.NUMBER)).toBe(3.14);
+        });
       });
     });
   });
+
 
   describe('function...', function() {
     it ('vanilla', function() {
@@ -56,5 +59,20 @@ describe('coerce...', function() {
         tc.coerce(function() {}, tc.CODES.STRING);
       }).toThrow();
     });
+  });
+
+  describe('array...', function() {
+    it ('string', function() {
+      var GOLD = '[]';
+      var empty = [];
+      expect(tc.coerce(empty, tc.CODES.STRING)).toBe(GOLD);
+    });
+
+    it ('string', function() {
+      var GOLD = '[1,2,3,4]';
+      var empty = [1,2,3,4];
+      expect(tc.coerce(empty, tc.CODES.STRING)).toBe(GOLD);
+    });
+
   });
 });
