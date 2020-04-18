3ee58aee21622ef4f524b0b3a526b8037430f4cf
Christopher Buteau <cbuteau@gmail.com>
2020-04-18 12:09:23 -0400

    do not cover track.js

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,10 @@ module.exports = function(config) {
     },
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors:  { '*.js': ['coverage'] },
+    preprocessors:  {
+      '*.js': ['coverage'],
+      '!track.js': ['coverage']
+    },
 
 
     // test results reporter to use
